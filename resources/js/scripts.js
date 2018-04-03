64fdb2caf4c6de35d9348bc078ed2060818e54bd
codeofrich <codeofrich@gmail.com>
2018-04-03 11:32:27 +0700

    adding scroll function to primary button

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    // For the sticky navigation
     $('.js--section-features').waypoint(function (direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -9,5 +10,10 @@ $(document).ready(function () {
     }, {
         offset: '60px'
     });
+
+    // Scroll on buttons
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000)
+    })
 
 });
